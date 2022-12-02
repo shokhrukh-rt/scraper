381603450dc6fe7b6465cba5ed5d0d316debc0e4
shokhrukh-rt <mr.shohruh.1003@gmail.com>
2022-12-03 08:15:06 +0900

    scraping test questions and answers

--- index.js
+++ index.js
@@ -8,9 +8,12 @@ const PORT = process.env.PORT || 5000;
 const xray = new Xray();
 app.use(express.json());
 
+const baseUrl = "https://www.englishtestsonline.com/";
+
+//this scrapes and gets all types of questions
 app.get("/", (req, res) => {
-	var stream = xray(
-		"https://www.englishtestsonline.com/english-grammar-mcq-test-with-answers-intermediate-09/",
+	const stream = xray(
+		`${baseUrl}english-grammar-mcq-test-with-answers-intermediate-07/`,
 		".mtq_question",
 		[
 			{
@@ -29,4 +32,24 @@ app.get("/", (req, res) => {
 	stream.pipe(res);
 });
 
+app.get("/vocabulary", (req, res) => {
+	const stream = xray(
+		`${baseUrl}conjunctions-and-adverbial-clauses-advanced-level-mcqs-test-with-answers-1/`,
+		".mtq_question",
+		[
+			{
+				qNumber: ".mtq_question_label",
+				qText: ".mtq_question_text",
+				answers: xray(".mtq_clickable", [
+					{
+						letterButton: ".mtq_css_letter_button",
+						trueFalse: ".mtq_marker@alt",
+						answerText: ".mtq_answer_text",
+					},
+				]),
+			},
+		]
+	).stream();
+	stream.pipe(res);
+});
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
