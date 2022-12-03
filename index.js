5b1fe2503dfedde7775f13b729333f610833887e
shokhrukh-rt <mr.shohruh.1003@gmail.com>
2022-12-04 07:16:35 +0900

    refactored some xray code

--- index.js
+++ index.js
@@ -8,48 +8,42 @@ const PORT = process.env.PORT || 5000;
 const xray = new Xray();
 app.use(express.json());
 
+//https://www.englishtestsonline.com/elementary-grammar-test-30/
+//https://www.englishtestsonline.com/advanced-level-grammar-exercise-mcq-test-20/
+
 const baseUrl = "https://www.englishtestsonline.com/";
+const intGrUrl = "english-grammar-mcq-test-with-answers-intermediate-";
+const elGrUrl = "elementary-grammar-test-";
+const adGrUrl = "advanced-level-grammar-exercise-mcq-test-";
+
+const scraper = (url, res) => {
+	const stream = xray(url, ".mtq_question", [
+		{
+			qNumber: ".mtq_question_label",
+			qText: ".mtq_question_text",
+			answers: xray(".mtq_clickable", [
+				{
+					letterButton: ".mtq_css_letter_button",
+					trueFalse: ".mtq_marker@alt",
+					answerText: ".mtq_answer_text",
+				},
+			]),
+		},
+	]).stream();
+	stream.pipe(res);
+};
 
 //this scrapes and gets all types of questions
-app.get("/", (req, res) => {
-	const stream = xray(
-		`${baseUrl}english-grammar-mcq-test-with-answers-intermediate-07/`,
-		".mtq_question",
-		[
-			{
-				qNumber: ".mtq_question_label",
-				qText: ".mtq_question_text",
-				answers: xray(".mtq_clickable", [
-					{
-						letterButton: ".mtq_css_letter_button",
-						trueFalse: ".mtq_marker@alt",
-						answerText: ".mtq_answer_text",
-					},
-				]),
-			},
-		]
-	).stream();
-	stream.pipe(res);
+app.get("/grammar/intermediate", (req, res) => {
+	scraper(`${baseUrl}${intGrUrl}07/`, res);
 });
 
-app.get("/vocabulary", (req, res) => {
-	const stream = xray(
-		`${baseUrl}conjunctions-and-adverbial-clauses-advanced-level-mcqs-test-with-answers-1/`,
-		".mtq_question",
-		[
-			{
-				qNumber: ".mtq_question_label",
-				qText: ".mtq_question_text",
-				answers: xray(".mtq_clickable", [
-					{
-						letterButton: ".mtq_css_letter_button",
-						trueFalse: ".mtq_marker@alt",
-						answerText: ".mtq_answer_text",
-					},
-				]),
-			},
-		]
-	).stream();
-	stream.pipe(res);
+app.get("/grammar/easy", (req, res) => {
+	scraper(`${baseUrl}${elGrUrl}30/`, res);
 });
+
+app.get("/grammar/advanced", (req, res) => {
+	scraper(`${baseUrl}${adGrUrl}20/`, res);
+});
+
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
